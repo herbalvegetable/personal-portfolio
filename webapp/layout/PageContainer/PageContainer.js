e6003ac1755f933429ae37cd35786f547346629f
Ben Aw Yong <herbalvegetable@gmail.com>
2023-12-08 19:23:19 +0800

    added nav sidebar

--- webapp/layout/PageContainer/PageContainer.js
+++ webapp/layout/PageContainer/PageContainer.js
@@ -1,9 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 import styles from './PageContainer.module.css';
 
 export default function PageContainer({ children }) {
+
+    const router = useRouter();
+
+    const [pageLinks, setPageLinks] = useState([
+        { href: '/', title: 'Home' },
+        { href: '/projects', title: 'Projects' },
+    ]);
+
+    const [links, setLinks] = useState([
+        { url: '' },
+    ]);
+
+    useEffect(() => {
+
+    }, []);
 
     return (
         <>
@@ -14,6 +30,32 @@ export default function PageContainer({ children }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className={styles.page_container}>
+
+                <div className={styles.sidebar}>
+                    <div className={styles.page_links}>
+                        {
+                            pageLinks.map((link, i) => {
+                                const { href, title } = link;
+
+
+                                return (
+                                    <div
+                                        className={styles.link}
+                                        onClick={e => router.push(href)}>
+                                        <div className={`
+                                            ${styles.active_container}
+                                            ${router.pathname === href ? styles.active : ''}
+                                        `}>
+                                            {title}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+
+                </div>
+
                 <div className={styles.middle}>
                     {children}
                 </div>
---
+++ webapp/src/pages/projects.js
@@ -0,0 +1,14 @@
+import { useState, useEffect, useRef } from 'react';
+
+import styles from '@/styles/Projects.module.css';
+
+import PageContainer from 'layout/PageContainer/PageContainer';
+
+export default function ProjectsPage(props){
+
+    return (
+        <PageContainer>
+            
+        </PageContainer>
+    )
+}
